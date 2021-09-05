849b96bcad6da1c134ae17105f810cd47357c76f
dcde22345 <dcde22345@gmail.com>
2021-09-06 00:54:19 +0800

    Adding nav and about application.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 window.onload = () => {
     const header = document.querySelector("header");
     const banner = document.querySelector(".banner");
+    const headShot = document.querySelector(".head-shot");
+    const slideRight = document.querySelectorAll(".slider-right");
+    const slideUp = document.querySelectorAll(".slider-up");
+    const slideLeft = document.querySelector(".slideLeft");
 
+    //nav animation
     const navOptions = {
         rootMargin: "-30% 0px 0px 0px",
     };
@@ -17,4 +22,48 @@ window.onload = () => {
 
     navScrolled.observe(banner);
 
+
+
+    //Slide right
+    const slideRightOptions = {
+        rootMargin: "0px 0px -50% 0px"
+    };
+
+    const slideRightScrolled = new IntersectionObserver(entries => {
+        entries.forEach((entry, slideRightScrolled) => {
+            if (entry.isIntersecting) {
+                slideRight.forEach((slider) => {
+                    slider.classList.add("slide-right");
+                })
+            } else {
+                return;
+            }
+        })
+    }, slideRightOptions);
+
+    slideRight.forEach((slider) => {
+        slideRightScrolled.observe(slider);
+    })
+
+
+
+    //Slide up
+    const slideUpOptions = {
+        rootMargin: "0px 0px -50% 0px"
+    };
+
+    const slideUpScrolled = new IntersectionObserver(entries => {
+        entries.forEach((entry, slideUpScrolled) => {
+            if (entry.isIntersecting) {
+                slideUp.forEach((slider) => {
+                    slider.classList.add("slide-up");
+                })
+            } else {
+                return;
+            }
+        })
+    }, slideUpOptions);
+    slideUp.forEach((slider) => {
+        slideUpScrolled.observe(slider);
+    })
 }
