85067d262d7c4b1d26c22db332b026119d085c90
dcde22345 <dcde22345@gmail.com>
2021-09-06 23:35:31 +0800

    Added a navbar application in RWD

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,59 @@
 window.onload = () => {
+    const body = document.body;
+    const wrap = document.querySelector(".wrap");
     const header = document.querySelector("header");
+    const navList = document.querySelector(".nav-list");
+    const navLinks = document.querySelectorAll(".nav-list li")
+    const burger = document.querySelector(".burger");
     const banner = document.querySelector(".banner");
     const headShot = document.querySelector(".head-shot");
     const slideRight = document.querySelectorAll(".slider-right");
     const slideUp = document.querySelectorAll(".slider-up");
     const slideLeft = document.querySelector(".slideLeft");
+
+
+    //burger toggle
+    function openMenu() {
+        navList.style.transform = "translateX(0)";
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinks 0.5s ease forwards ${(index + 1) / 5}s`;
+        });
+        isMenuOpen = true;
+    }
+
+    function closeMenu() {
+        navList.style.transform = "translateX(-100%)";
+        navLinks.forEach((link) => {
+            link.style.animation = "";
+        })
+        isMenuOpen = false;
+    }
+
+    function isClickingBurger(event) {
+        if (event.target === burger) {
+            openMenu();
+        }
+    }
+
+    function isClickingNavList(event) {
+        if (event.target !== navList) {
+            closeMenu()
+        }
+    }
+
+    let isMenuOpen = false;
+
+    wrap.addEventListener("click", (event) => {
+        if (!isMenuOpen) {
+            isClickingBurger(event);
+        } else {
+            isClickingNavList(event);
+        }
+    })
+
+
+
+    //Scrolled animation
 
     //nav animation
     const navOptions = {
